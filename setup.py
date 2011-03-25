0c04118b76ff087949afcb6e4213b82820f10642
Christopher Allan Webber <cwebber@dustycloud.org>
2011-03-24 19:00:55 -0500

    Adding requirements :)

--- setup.py
+++ setup.py
@@ -7,11 +7,22 @@ setup(
     version = "0.0.1",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     zip_safe=False,
+    # scripts and dependencies
+    install_requires = [
+        'setuptools',
+        'PasteScript',
+        'beaker',
+        'routes',
+        'pymongo',
+        'webob',
+        'wtforms',
+        ],
+
     license = 'AGPLv3',
     author = 'Christopher Webber',
     author_email = 'cwebber@dustycloud.org',
     entry_points = """\
       [paste.app_factory]
-      mediagoblin = mediagoblin.app:paste_app_factory
+      app = mediagoblin.app:paste_app_factory
       """,
     )
