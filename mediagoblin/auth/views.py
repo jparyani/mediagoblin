db1a438f3e6f8c5c8cec20b9326a21baf4579306
Joar Wandborg <git@wandborg.com>
2011-05-03 19:49:39 +0200

    Added functionality to support user email verification, email = TBD, verification = done. Signed-off-by: Joar Wandborg <git@wandborg.com>

--- mediagoblin/auth/views.py
+++ mediagoblin/auth/views.py
@@ -116,3 +116,26 @@ def logout(request):
     
     return exc.HTTPFound(
         location=request.urlgen("index"))
+
+def verify_email(request):
+    import bson.objectid
+    user = request.db.User.find_one(
+        {'_id': bson.objectid.ObjectId( unicode( request.GET.get('userid') ) )})
+
+    verification_successful = bool
+
+    if user and user['verification_key'] == unicode( request.GET.get('token') ):
+        user['status'] = u'active'
+        user['email_verified'] = True
+        verification_successful = True
+        user.save()
+    else:
+        verification_successful = False
+        
+    template = request.template_env.get_template(
+        'mediagoblin/auth/verify_email.html')
+    return Response(
+        template.render(
+            {'request': request,
+             'user': user,
+             'verification_successful': verification_successful}))
