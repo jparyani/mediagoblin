dbb92c602075c768d88d0c21b774d75203af3fc1
Christopher Allan Webber <cwebber@dustycloud.org>
2011-05-22 09:25:51 -0500

    Move the ./bin/gmg shell command into its own module.

--- mediagoblin/gmg_commands/__init__.py
+++ mediagoblin/gmg_commands/__init__.py
@@ -14,70 +14,18 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import code
 import argparse
-import os
 
-from paste.deploy.loadwsgi import NicerConfigParser
-
-from mediagoblin.celery_setup import setup_celery_from_config
-from mediagoblin import app, util
-from mediagoblin import globals as mgoblin_globals
+from mediagoblin import util
 
 
 SUBCOMMAND_MAP = {
     'shell': {
-        'setup': 'mediagoblin.gmg_commands:shell_parser_setup',
-        'func': 'mediagoblin.gmg_commands:shell',
+        'setup': 'mediagoblin.gmg_commands.shell:shell_parser_setup',
+        'func': 'mediagoblin.gmg_commands.shell:shell',
         'help': 'Run a shell with some tools pre-setup'},
     }
 
-
-def shell_parser_setup(subparser):
-    subparser.add_argument(
-        '-cf', '--conf_file', default='mediagoblin.ini',
-        help="Config file used to set up environment")
-    subparser.add_argument(
-        '-cs', '--app_section', default='app:mediagoblin',
-        help="Section of the config file where the app config is stored.")
-
-
-SHELL_BANNER = """\
-GNU MediaGoblin shell!
-----------------------
-Available vars:
- - mgoblin_app: instantiated mediagoblin application
- - mgoblin_globals: mediagoblin.globals
- - db: database instance
-"""
-
-
-def shell(args):
-    """
-    """
-    # Duplicated from from_celery.py, remove when we have the generic util
-    parser = NicerConfigParser(args.conf_file)
-    parser.read(args.conf_file)
-    parser._defaults.setdefault(
-        'here', os.path.dirname(os.path.abspath(args.conf_file)))
-    parser._defaults.setdefault(
-        '__file__', os.path.abspath(args.conf_file))
-
-    mgoblin_section = dict(parser.items(args.app_section))
-    mgoblin_conf = dict(
-        [(section_name, dict(parser.items(section_name)))
-         for section_name in parser.sections()])
-
-    mgoblin_app = app.paste_app_factory(
-        mgoblin_conf, **mgoblin_section)
-
-    code.interact(
-        banner=SHELL_BANNER,
-        local={
-            'mgoblin_app': mgoblin_app,
-            'mgoblin_globals': mgoblin_globals,
-            'db': mgoblin_globals.database})
-    
 
 def main_cli():
     parser = argparse.ArgumentParser(
