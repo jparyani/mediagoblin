78e1c5f1edf7c5e651f40921bd69448a4ceec9eb
Christopher Allan Webber <cwebber@dustycloud.org>
2011-06-19 13:00:49 -0500

    Actually we need pkg_resources again in test_celery_setup :)
    This got removed in master at the same time that it got used in the
    configobj branch...

--- mediagoblin/tests/test_celery_setup.py
+++ mediagoblin/tests/test_celery_setup.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import pkg_resources
+
 from mediagoblin import celery_setup
 from mediagoblin.config import read_mediagoblin_config
 
