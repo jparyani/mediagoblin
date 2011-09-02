12a100e4d8bdda7bd2353403a7e08e3a94669498
Will Kahn-Greene <willg@bluesock.org>
2011-09-01 20:49:54 -0400

    508. Updates copyright/license information

--- mediagoblin/user_pages/views.py
+++ mediagoblin/user_pages/views.py
@@ -1,5 +1,5 @@
 # MediaGoblin -- federated, autonomous media hosting
-# Copyright (C) 2011 Free Software Foundation, Inc
+# Copyright (C) 2011 MediaGoblin contributors.  See AUTHORS.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
