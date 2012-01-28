9542a2ba076b7e00e79d7adb1a4e90a095427645
Joar Wandborg <git@wandborg.com>
2012-01-28 01:31:56 +0100

    JavaScript fixes - AGPL headers, etc
    - Added AGPL header to comment_show.js, show_password.js
      and geolocation-map.js
    - Removed console.log from geolocation-map.js

--- mediagoblin/static/js/geolocation-map.js
+++ mediagoblin/static/js/geolocation-map.js
@@ -1,11 +1,28 @@
+/**
+ * GNU MediaGoblin -- federated, autonomous media hosting
+ * Copyright (C) 2011 MediaGoblin contributors.  See AUTHORS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $(document).ready(function () {
     var longitude = Number(
 	$('#tile-map #gps-longitude').val());
     var latitude = Number(
 	$('#tile-map #gps-latitude').val());
 
-    console.log(longitude, latitude);
-
+    // Get a new map instance attached and element with id="tile-map"
     var map = new L.Map('tile-map');
 
     var mqtileUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
@@ -21,7 +38,7 @@ $(document).ready(function () {
 	 attribution: mqtileAttrib,
 	 subdomains: '1234'});
 
-    var location = new L.LatLng(latitude, longitude); // geographical point (longitude and latitude)
+    var location = new L.LatLng(latitude, longitude); 
     map.setView(location, 13).addLayer(mqtile);
 
     var marker = new L.Marker(location);
