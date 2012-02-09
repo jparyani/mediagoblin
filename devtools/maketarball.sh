df900eed1718d9bb83c3289f00fdb539fa9b5063
Will Kahn-Greene <willg@bluesock.org>
2012-02-09 10:20:42 -0500

    Update copyright headers for shell scripts

--- devtools/maketarball.sh
+++ devtools/maketarball.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # GNU MediaGoblin -- federated, autonomous media hosting
-# Copyright (C) 2011 Free Software Foundation, Inc
+# Copyright (C) 2011, 2012 GNU MediaGoblin Contributors.  See AUTHORS.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
