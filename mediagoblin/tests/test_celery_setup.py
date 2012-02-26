a9a63a686e934e3189179dcf01faad39a330a378
Christopher Allan Webber <cwebber@dustycloud.org>
2012-02-26 15:45:58 -0600

    Adjust unit tests to match new celery/kombu sqlalchemy setup

--- mediagoblin/tests/test_celery_setup.py
+++ mediagoblin/tests/test_celery_setup.py
@@ -22,8 +22,6 @@ from mediagoblin.init.config import read_mediagoblin_config
 
 TEST_CELERY_CONF_NOSPECIALDB = pkg_resources.resource_filename(
     'mediagoblin.tests', 'fake_celery_conf.ini')
-TEST_CELERY_CONF_MGSPECIALDB = pkg_resources.resource_filename(
-    'mediagoblin.tests', 'fake_celery_conf_mgdb.ini')
 
 
 def test_setup_celery_from_config():
@@ -51,35 +49,12 @@ def test_setup_celery_from_config():
     assert fake_celery_module.CELERY_RESULT_PERSISTENT is True
     assert fake_celery_module.CELERY_IMPORTS == [
         'foo.bar.baz', 'this.is.an.import', 'mediagoblin.processing']
-    assert fake_celery_module.CELERY_MONGODB_BACKEND_SETTINGS == {
-        'database': 'mediagoblin'}
-    assert fake_celery_module.CELERY_RESULT_BACKEND == 'mongodb'
-    assert fake_celery_module.BROKER_BACKEND == 'mongodb'
+    assert fake_celery_module.CELERY_RESULT_BACKEND == 'database'
+    assert fake_celery_module.CELERY_RESULT_DBURI == (
+        'sqlite:///' +
+        pkg_resources.resource_filename('mediagoblin.tests', 'celery.db'))
 
-    _wipe_testmodule_clean(fake_celery_module)
-
-    global_config, validation_result = read_mediagoblin_config(
-        TEST_CELERY_CONF_MGSPECIALDB)
-    app_config = global_config['mediagoblin']
-
-    celery_setup.setup_celery_from_config(
-        app_config, global_config,
-        'mediagoblin.tests.fake_celery_module', set_environ=False)
-    
-    from mediagoblin.tests import fake_celery_module
-    assert fake_celery_module.SOME_VARIABLE == 'poolf'
-    assert fake_celery_module.MAIL_PORT == 2020
-    assert isinstance(fake_celery_module.MAIL_PORT, int)
-    assert fake_celery_module.CELERYD_ETA_SCHEDULER_PRECISION == 3.1
-    assert isinstance(fake_celery_module.CELERYD_ETA_SCHEDULER_PRECISION, float)
-    assert fake_celery_module.CELERY_RESULT_PERSISTENT is False
-    assert fake_celery_module.CELERY_IMPORTS == [
-        'baz.bar.foo', 'import.is.a.this', 'mediagoblin.processing']
-    assert fake_celery_module.CELERY_MONGODB_BACKEND_SETTINGS == {
-        'database': 'captain_lollerskates',
-        'host': 'mongodb.example.org',
-        'port': 8080}
-    assert fake_celery_module.CELERY_RESULT_BACKEND == 'mongodb'
-    assert fake_celery_module.BROKER_BACKEND == 'mongodb'
-    assert fake_celery_module.BROKER_HOST == 'mongodb.example.org'
-    assert fake_celery_module.BROKER_PORT == 8080
+    assert fake_celery_module.BROKER_TRANSPORT == 'sqlalchemy'
+    assert fake_celery_module.BROKER_HOST == (
+        'sqlite:///' +
+        pkg_resources.resource_filename('mediagoblin.tests', 'kombu.db'))
