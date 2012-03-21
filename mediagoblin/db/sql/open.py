e136ad3ed613b1c2b42093be687e3aea9889e5b3
Elrond <elrond+mediagoblin.org@samba-tng.org>
2012-03-21 21:46:07 +0100

    Finally load all models.
    Load all models for the media_types. This was stopped by a
    celery problem. But that is now fixed.

--- mediagoblin/db/sql/open.py
+++ mediagoblin/db/sql/open.py
@@ -45,8 +45,7 @@ class DatabaseMaster(object):
 def load_models(app_config):
     import mediagoblin.db.sql.models
 
-    # TODO/Fix: This breaks celery
-    if False:
+    if True:
         for media_type in app_config['media_types']:
             _log.debug("Loading %s.models", media_type)
             __import__(media_type + ".models")
