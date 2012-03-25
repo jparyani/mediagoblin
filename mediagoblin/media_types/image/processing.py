763ef5b77eaa8f6f172c26f6cb2d985dcd330f44
Elrond <elrond+mediagoblin.org@samba-tng.org>
2012-03-25 14:12:53 +0200

    Start to fill media_data.exif_all in addition.
    In addition to filling the normal slot media_data['exif'],
    now also fill media_data.exif_all. This is the new slot
    used by SQL.
    For a few moments this will create duplicated entries in
    the mongo db. But this shouldn't hurt.

--- mediagoblin/media_types/image/processing.py
+++ mediagoblin/media_types/image/processing.py
@@ -114,14 +114,17 @@ def process_image(entry):
     media_files_dict['medium'] = medium_filepath
 
     # Insert exif data into database
+    exif_all = clean_exif(exif_tags)
     media_data = entry.setdefault('media_data', {})
 
     # TODO: Fix for sql media_data, when exif is in sql
     if media_data is not None:
         media_data['exif'] = {
-            'clean': clean_exif(exif_tags)}
-        media_data['exif']['useful'] = get_useful(
-            media_data['exif']['clean'])
+            'clean': exif_all}
+        media_data['exif']['useful'] = get_useful(exif_all)
+
+    if len(exif_all):
+        entry.media_data_init(exif_all=exif_all)
 
     if len(gps_data):
         for key in list(gps_data.keys()):
