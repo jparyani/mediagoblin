ec05785089144e6bf7e2dd9056774342cec1415b
Christopher Allan Webber <cwebber@dustycloud.org>
2012-04-21 20:29:02 -0500

    Remove kombu-sqlalchemy and use database string in modern kombu-proper format
    Previously we were using kombu-sqlalchemy the library for sqlalchemy
    support.  Newer kombu has support for this internally, so we're using
    that.  Requires changing a celery setting and also removing
    kombu-sqlalchemy from your path.
    
    I hope the process of removing kombu-sqlalchemy from one's path
    doesn't confuse too many people...!

--- setup.py
+++ setup.py
@@ -64,7 +64,6 @@ setup(
         'Markdown',
         'sqlalchemy',
         'sqlalchemy-migrate',
-        'kombu-sqlalchemy',
         ## For now we're expecting that users will install this from
         ## their package managers.
         # 'lxml',
