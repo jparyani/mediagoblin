1c2d01ae3ba421536d5775e5992393019714b856
Christopher Allan Webber <cwebber@dustycloud.org>
2013-01-25 10:39:20 -0600

    Very start of plugin hooks and openstreetmap pluginification
     - Added start of template hook code to pluginapi.py
     - Started to break openstreetmap into plugin; moved templates
     - Added plugin hooks in media and image media templates
    
    ... almost certainly, none of this works yet. :)

--- mediagoblin/tools/pluginapi.py
+++ mediagoblin/tools/pluginapi.py
@@ -83,6 +83,9 @@ class PluginManager(object):
         # list of registered template paths
         "template_paths": set(),
 
+        # list of template hooks
+        "template_hooks": {},
+
         # list of registered routes
         "routes": [],
         }
@@ -130,6 +133,28 @@ class PluginManager(object):
 
     def get_routes(self):
         return tuple(self.routes)
+
+    def register_template_hooks(self, template_hooks):
+        """
+        Register a dict of template hooks.
+
+        Takes template_hooks as an argument, which is a dictionary of
+        template hook names/keys to the templates they should provide.
+        (The value can either be a single template path or an iterable
+        of paths.)
+
+        Example:
+          {"media_sidebar": "/plugin/sidemess/mess_up_the_side.html",
+           "media_descriptionbox": ["/plugin/sidemess/even_more_mess.html",
+                                    "/plugin/sidemess/so_much_mess.html"]}
+        """
+        for hook, templates in template_hooks.items():
+            if isinstance(templates, (list, tuple)):
+                self.template_hooks.setdefault(hook, []).extend(list(templates))
+            else:
+                # In this case, it's actually a single callable---not a
+                # list of callables.
+                self.hooks.setdefault(hook, []).append(templates)
 
 
 def register_routes(routes):
@@ -208,3 +233,5 @@ def get_config(key):
     return plugin_section.get(key, {})
 
 
+def register_template_hooks(template_hooks):
+    PluginManager().register_template_hooks(template_hooks)
