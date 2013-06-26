4f8f0a4e1f2d5a1eb64f47e62958d77bd12379f3
Rodney Ewing <ewing.rj@gmail.com>
2013-06-26 12:04:45 -0700

    merge --squash persona branch to take care of a false merge commit in the basic_auth branch that persona is forked from
    Conflicts:
    	mediagoblin/templates/mediagoblin/auth/login.html
    	mediagoblin/templates/mediagoblin/auth/register.html
    	mediagoblin/templates/mediagoblin/edit/edit_account.html
    
    These are commit messages from the squashed persona stuff:
    
     - added tests and fixed minor errors
     - fixed a redirect loop when only persona is enabled and accessing /auth/login
     - moved persona.js to plugin's static dir
     - fixes for add/remove persona emails
     - add and remove personas
     - working with multiple plugins
     - working version
     - switched to hidden form instead of ajax
     - beginings

---
+++ mediagoblin/plugins/persona/forms.py
@@ -0,0 +1,41 @@
+# GNU MediaGoblin -- federated, autonomous media hosting
+# Copyright (C) 2011, 2012 MediaGoblin contributors.  See AUTHORS.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import wtforms
+
+from mediagoblin.tools.translate import lazy_pass_to_ugettext as _
+from mediagoblin.auth.tools import normalize_user_or_email_field
+
+
+class RegistrationForm(wtforms.Form):
+    username = wtforms.TextField(
+        _('Username'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_email=False)])
+    email = wtforms.TextField(
+        _('Email address'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_user=False)])
+    persona_email = wtforms.HiddenField(
+        '',
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_user=False)])
+
+
+class EditForm(wtforms.Form):
+    email = wtforms.TextField(
+        _('Email address'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_user=False)])
