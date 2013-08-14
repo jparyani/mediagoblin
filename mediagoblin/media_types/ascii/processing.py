d63f78fad9f64646c2cc1917862605bd4a1e55ba
Rodney Ewing <ewing.rj@gmail.com>
2013-08-14 15:11:03 -0700

    forgot to add the ascii resizer to the list of processors

--- mediagoblin/media_types/ascii/processing.py
+++ mediagoblin/media_types/ascii/processing.py
@@ -242,3 +242,4 @@ class AsciiProcessingManager(ProcessingManager):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.add_processor(InitialProcessor)
+        self.add_processor(Resizer)
