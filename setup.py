904caab6b17a7030271d4eca3b0eea8e51a123fc
Christopher Allan Webber <cwebber@dustycloud.org>
2013-11-10 12:38:49 -0600

    Apparently kombu is fine again

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'pytest-xdist',
         'werkzeug>=0.7',
         'celery>=3.0',
-        'kombu<3.0',
+        'kombu',
         'jinja2',
         'sphinx',
         'Babel<1.0',
