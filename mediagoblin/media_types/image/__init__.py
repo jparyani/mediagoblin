52a0387fc6ad023516c765019725ca58daaae347
Sebastian Spaeth <Sebastian@SSpaeth.de>
2013-12-12 17:29:48 +0100

    Use MEDIA_TYPE, not hardcoded variable of same value

--- mediagoblin/media_types/image/__init__.py
+++ mediagoblin/media_types/image/__init__.py
@@ -28,7 +28,7 @@ ACCEPTED_EXTENSIONS = ["jpg", "jpeg", "png", "gif", "tiff"]
 MEDIA_TYPE = 'mediagoblin.media_types.image'
 
 def setup_plugin():
-    config = pluginapi.get_config('mediagoblin.media_types.image')
+    config = pluginapi.get_config(MEDIA_TYPE)
 
 class ImageMediaManager(MediaManagerBase):
     human_readable = "Image"
