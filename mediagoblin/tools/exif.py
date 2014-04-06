bfb99d65f4e92d018a73b67bed2cea9d629a41e2
Christopher Allan Webber <cwebber@dustycloud.org>
2014-04-06 13:58:47 -0500

    No more EXIF, for real.  Use exifread.  This should fix recent import EXIF failures.
    This commit sponsored by Aaron Whitehouse.  Thank you!

--- mediagoblin/tools/exif.py
+++ mediagoblin/tools/exif.py
@@ -14,10 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-sys.path.append('extlib/exif')
-
-from EXIF import process_file
+from exifread import process_file
 from exifread.utils import Ratio
 
 from mediagoblin.processing import BadMediaFail
