0b2572b9a8f8d1e22e724283cd99152c767208e8
Berker Peksag <berker.peksag@gmail.com>
2014-06-02 21:01:48 +0300

    Add py2_unicode helper to mediagoblin._compat.

--- mediagoblin/_compat.py
+++ mediagoblin/_compat.py
@@ -14,3 +14,10 @@ else:
     import urlparse
     ugettext = mg_globals.thread_scope.translations.ugettext
     ungettext = mg_globals.thread_scope.translations.ungettext
+
+
+def py2_unicode(klass):
+    if not PY3:
+        klass.__unicode__ = klass.__str__
+        klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
+    return klass
