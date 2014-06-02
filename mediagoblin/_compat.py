dce76c3ee7153e8340cb3d64e8ee4df23a545bc4
Berker Peksag <berker.peksag@gmail.com>
2014-06-03 01:41:44 +0300

    Sync py2_unicode decorator with Django.

--- mediagoblin/_compat.py
+++ mediagoblin/_compat.py
@@ -1,5 +1,3 @@
-import sys
-
 from six import PY3, iteritems
 
 from mediagoblin import mg_globals
@@ -17,8 +15,13 @@ else:
     ungettext = mg_globals.thread_scope.translations.ungettext
 
 
+# taken from https://github.com/django/django/blob/master/django/utils/encoding.py
 def py2_unicode(klass):
     if not PY3:
+        if '__str__' not in klass.__dict__:
+            raise ValueError("@py2_unicode cannot be applied "
+                             "to %s because it doesn't define __str__()." %
+                             klass.__name__)
         klass.__unicode__ = klass.__str__
         klass.__str__ = lambda self: self.__unicode__().encode('utf-8')
     return klass
