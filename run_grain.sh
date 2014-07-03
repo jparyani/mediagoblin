921f31ef42913034dcbd7da2285e374e0e937ea2
Jason Paryani <github@jparyani.com>
2014-07-03 01:03:19 -0700

    Add a symlink to user_dev, since setting the config doesn't seem to work

--- run_grain.sh
+++ run_grain.sh
@@ -4,7 +4,8 @@ cp -r /etc/service /tmp
 test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
-test -f /var/lock || ln -s /var/run/lock /var/lock
+test -d /var/lock || ln -s /var/run/lock /var/lock
 test -f /var/mediagoblin.db || cp /var_original/mediagoblin.db /var
+mkdir -p /var/user_dev
 
 /sbin/my_init
