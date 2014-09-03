5efee4c316f03c090fc5b8d2ebff0f37184eec77
Jason Paryani <github@jparyani.com>
2014-09-02 23:27:43 -0700

    Bump version and handle db update

--- run_grain.sh
+++ run_grain.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 export HOME=/var
 export TMPDIR=/var/tmp
 
@@ -8,9 +10,11 @@ cp -r /etc/service /tmp
 test -d /var/log || cp -r /var_original/log /var
 test -d /var/lib || cp -r /var_original/lib /var
 test -d /var/run || cp -r /var_original/run /var
-test -d /var/lock || ln -s /var/run/lock /var/lock
+test -e /var/lock || ln -s /var/run/lock /var/lock
 test -f /var/mediagoblin.db || cp /var_original/mediagoblin.db /var
 mkdir -p /var/user_dev
+
+test -f /var/version || (echo '1' > /var/version && /opt/app/bin/gmg dbupdate)
 
 cd /opt/app
 ./lazyserver.sh --server-name=broadcast 2>&1
