fa3f46d7144629eed6f1f6f9abc884a1b8f4c330
Christopher Allan Webber <cwebber@dustycloud.org>
2014-09-16 14:46:02 -0500

    Import mock correctly on py3
    This commit sponsored by Andrew McNicol.  Thank you!

--- mediagoblin/tests/test_util.py
+++ mediagoblin/tests/test_util.py
@@ -14,7 +14,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import mock
+try:
+    import mock
+except ImportError:
+    import unittest.mock as mock
 import email
 import pytest
 import smtplib
